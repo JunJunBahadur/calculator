5409267cbb6c62043f07c9eea7869cae15d6229f
Arjun Senthil <arjunsenthilks2001@gmail.com>
2021-07-24 13:55:47 +0530

    All done only deco left

--- calc.js
+++ calc.js
@@ -1,8 +1,10 @@
 let a = 0,
     b = 0,
     operator = "",
+    prevOperator = "",
     flagResDisp = true, //This is to refresh the result display for the second number
     flagFirstRun = true, //Checks if it's the first calculation
+    multipleDots = false,
     allowEqual = true;
 
 const calculatorDisp = document.querySelector(".calculatorDisp");
@@ -10,9 +12,9 @@ const resultDisp = document.querySelector(".resultDisp");
 
 
 const clearAll = document.querySelector(".clearAll");
-clearAll.addEventListener('click', resetAll());
+clearAll.addEventListener('click', resetAll);
 
-function resetAll() {
+function resetAll() { //Resets all variables and displays
     a = 0;
     b = 0;
     resultDisp.textContent = "";
@@ -21,8 +23,15 @@ function resetAll() {
     flagResDisp = true;
     flagFirstRun = true;
     allowEqual = true;
+    multipleDots = false;
 }
 
+const clearOne = document.querySelector(".clearOne");
+clearOne.addEventListener('click', () => { //Clears one element at hte end of the result display
+    let arr = resultDisp.textContent.split("");
+    arr.pop();
+    resultDisp.textContent = arr.join("");
+});
 
 const numbersPressed = document.querySelectorAll(".number");
 numbersPressed.forEach(numberPressed => {
@@ -37,7 +46,16 @@ function getValueNumber() { //This prints the number on result display
         allowEqual = true;
         flagResDisp = true;
         noRepeatEqual();
+    } else if (allowEqual == false) {
+        resetAll();
+    } else if (multipleDots && arguments[0] == '.') {
+        return;
     }
+
+    if (arguments[0] == '.') {
+        multipleDots = true;
+    }
+
     resultDisp.textContent += String(arguments[0]);
 }
 
@@ -52,15 +70,28 @@ operatorsPressed.forEach(operatorPressed => {
 function getValueOperator() { //Gets the operator value and gets the second value
     operator = String(arguments[0]);
     b = Number(resultDisp.textContent);
+
+    if (prevOperator == "/" && b === 0) {
+        alert("You can't divide by 0!");
+        resetAll();
+        return;
+    }
+
     calculatorDisp.textContent += resultDisp.textContent + " " + operator + " ";
     if (!flagFirstRun) {
-        a = operate(operator, a, b);
-        resultDisp.textContent = a;
+        a = operate(prevOperator, a, b);
+        if (multipleDots) {
+            resultDisp.textContent = a.toFixed(2);
+        } else {
+            resultDisp.textContent = a;
+        }
     } else {
         a = b;
     }
+    prevOperator = operator;
     flagResDisp = false;
     flagFirstRun = false;
+    multipleDots = false;
 }
 
 
@@ -74,12 +105,22 @@ function noRepeatEqual() { //This function makes sure that the equal symbol can'
     }
 }
 
-function callingEqual() {
+function callingEqual() { //Calculates when equal is pressed
     b = Number(resultDisp.textContent);
-    calculatorDisp.textContent += resultDisp.textContent + " ";
-    resultDisp.textContent = operate(operator, a, b);
+    if (operator == "/" && b === 0) {
+        alert("You can't divide by 0!");
+        resetAll();
+        return;
+    }
+    calculatorDisp.textContent += resultDisp.textContent + " " + "=";
+    if (multipleDots) {
+        resultDisp.textContent = operate(operator, a, b).toFixed(2);
+    } else {
+        resultDisp.textContent = operate(operator, a, b);
+    }
     a = Number(resultDisp.textContent);
     allowEqual = false;
+    flagFirstRun = true;
     noRepeatEqual();
 }
 
@@ -102,6 +143,9 @@ function operate(operator, number1, number2) { //Performs basic mathmematic oper
         case '/':
             result = number1 / number2;
             break;
+
+        case '^':
+            result = Math.pow(number1,number2);
     }
     return result;
 }
